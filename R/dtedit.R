60f218d33d65a26300551c6930f742005a0d16a0
Gregory R. Warnes <greg@warnes.net>
2024-07-09 14:23:22 -0400

    Add `input` to callback parameters.

--- R/dtedit.R
+++ R/dtedit.R
@@ -1,18 +1,20 @@
 #' Function to create a DataTable with Add, Edit, and Delete buttons.
 #'
-#' This object will maintain data state. However, in order of the data to persist
-#' between Shiny instances, data needs to be saved to some external format (e.g.
-#' database or R data file). The callback functions provide a mechanism for this
-#' function to interact with a permanent data storage scheme. The callback
-#' functions are called when the user adds, updates, or deletes a row from the
-#' data table. The callback must accept two parameters: \code{data} and \code{row}.
-#' For inserting and updating, the \code{data} object is the current state of
-#' data table including any additions or updates. The \code{row} parameter indicates
-#' which row from \code{data} was modified (or added). For deletions, however,
-#' the \code{data} represents the data table just before deleting the specified
-#' row. That is, if \code{callback.delete} returns a \code{data.frame}, that will
-#' be the new data table; otherwise this function will remove row \code{row} from
-#' \code{data} and that will become the current data table.
+#' This object will maintain data state. However, in order of the data to
+#' persist between Shiny instances, data needs to be saved to some external
+#' format (e.g. database or R data file). The callback functions provide a
+#' mechanism for this function to interact with a permanent data storage scheme.
+#'
+#' The callback functions are called when the user adds, updates, or deletes a
+#' row from the data table. The callback must accept three parameters:
+#' \code{data}, \code{row}, and \code{input} For inserting and updating, the
+#' \code{data} object is the current state of data table including any additions
+#' or updates. The \code{row} parameter indicates which row from \code{data} was
+#' modified (or added). For deletions, however, the \code{data} represents the
+#' data table just before deleting the specified row. That is, if
+#' \code{callback.delete} returns a \code{data.frame}, that will be the new data
+#' table; otherwise this function will remove row \code{row} from \code{data}
+#' and that will become the current data table.
 #'
 #' The callback functions may throw errors (see e.g. \code{stop}) if there are
 #' problems with data. That is, if data validation checks indicate data problems
@@ -22,35 +24,37 @@
 #' modal dialog is not dismissed and the user can further edit the data and
 #' retry the insertion or update.
 #'
-#' Callback functions may return a \code{data.frame}. When a \code{data.frame} is
-#' returned that will become the current state of the data table. If anything
+#' Callback functions may return a \code{data.frame}. When a \code{data.frame}
+#' is returned that will become the current state of the data table. If anything
 #' else is returned then the internal \code{data.frame} will be used.
 #'
 #' @param input Shiny input object passed from the server.
 #' @param output Shiny output object passed from the server.
-#' @param name the name of the UI output. That is, put \code{uiOutput(name)} where
-#'        you want the DataTable in \code{ui.R}. When using more that one \code{dtedit}
-#'        within a Shiny application the name must be unique.
+#' @param name the name of the UI output. That is, put \code{uiOutput(name)}
+#'   where you want the DataTable in \code{ui.R}. When using more that one
+#'   \code{dtedit} within a Shiny application the name must be unique.
 #' @param thedata a data frame to view and edit.
-#' @param view.cols character vector with the column names to show in the DataTable.
-#'        This can be a subset of the full \code{data.frame}.
-#' @param edit.cols character vector with the column names the user can edit/add.
-#'        This can be a subset of the full \code{data.frame}.
+#' @param view.cols character vector with the column names to show in the
+#'   DataTable. This can be a subset of the full \code{data.frame}.
+#' @param edit.cols character vector with the column names the user can
+#'   edit/add. This can be a subset of the full \code{data.frame}.
 #' @param edit.label.cols character vector with the labels to use on the edit
-#'        and add dialogs. The length and order of \code{code.cols.labels} must
-#'        correspond to \code{edit.cols}.
+#'   and add dialogs. The length and order of \code{code.cols.labels} must
+#'   correspond to \code{edit.cols}.
 #' @param input.types a character vector where the name corresponds to a column
-#'        in \code{edit.cols} and the value is the input type. Possible values
-#'        are \code{dateInput}, \code{selectInput}, \code{numericInput},
-#'        \code{textInput}, \code{textAreaInput}, or \code{passwordInput}.
-#'        The most common case where this parameter is desirable is when a text
-#'        area is required instead of a simple text input.
-#' @param input.choices a list of character vectors. The names of each element in the list must
-#'        correspond to a column name in the data. The value, a character vector, are the options
-#'        presented to the user for data entry.
-#' @param selectize Whether to use selectize.js or not. See \code{\link{selectInput}} for more info.
+#'   in \code{edit.cols} and the value is the input type. Possible values are
+#'   \code{dateInput}, \code{selectInput}, \code{numericInput},
+#'   \code{textInput}, \code{textAreaInput}, or \code{passwordInput}. The most
+#'   common case where this parameter is desirable is when a text area is
+#'   required instead of a simple text input.
+#' @param input.choices a list of character vectors. The names of each element
+#'   in the list must correspond to a column name in the data. The value, a
+#'   character vector, are the options presented to the user for data entry.
+#' @param selectize Whether to use selectize.js or not. See
+#'   \code{\link{selectInput}} for more info.
 #' @param defaultPageLength number of rows to show in the data table by default.
-#' @param modal.size the size of the modal dialog. See \code{\link{modalDialog}}.
+#' @param modal.size the size of the modal dialog. See
+#'   \code{\link{modalDialog}}.
 #' @param text.width width of text inputs.
 #' @param textarea.width the width of text area inputs.
 #' @param textarea.height the height of text area inputs.
@@ -60,31 +64,32 @@
 #' @param title.delete the title of the dialog box for deleting a row.
 #' @param title.edit the title of the dialog box for editing a row.
 #' @param title.add the title of the dialog box for inserting a new row.
-#' 
+#'
 #' @param label.delete the label of the delete button.
 #' @param label.edit the label of the edit button.
 #' @param label.add the label of the add button.
 #' @param label.copy the label of the copy button.
-#' 
+#'
 #' @param show.delete whether to show/enable the delete button.
 #' @param show.update whether to show/enable the update button.
 #' @param show.insert whether to show/enable the insert button.
 #' @param show.copy whether to show/enablre the copy button.
-#' 
-#' @param callback.delete a function called when the user deletes a row. This function should
-#'        return an updated data.frame.
-#' @param callback.update a function called when the user updates a row. This function should
-#'        return an updated data.frame.
-#' @param callback.insert a function called when the user inserts a new row. This function should
-#'        return an updated data.frame.
-#' @param click.time.threshold This is to prevent duplicate entries usually by double clicking the
-#'        save or update buttons. If the user clicks the save button again within this amount of
-#'        time (in seconds), the subsequent click will be ignored. Set to zero to disable this
-#'        feature. For developers, a message is printed using the warning function.
+#'
+#' @param callback.delete a function called when the user deletes a row. This
+#'   function should return an updated data.frame.
+#' @param callback.update a function called when the user updates a row. This
+#'   function should return an updated data.frame.
+#' @param callback.insert a function called when the user inserts a new row.
+#'   This function should return an updated data.frame.
+#' @param click.time.threshold This is to prevent duplicate entries usually by
+#'   double clicking the save or update buttons. If the user clicks the save
+#'   button again within this amount of time (in seconds), the subsequent click
+#'   will be ignored. Set to zero to disable this feature. For developers, a
+#'   message is printed using the warning function.
 #' @param datatable.options options passed to \code{\link{DT::renderDataTable}}.
-#'        See \link{https://rstudio.github.io/DT/options.html} for more information.
-#' @inheritParams DT::renderDataTable      
-#'  
+#'   See \link{https://rstudio.github.io/DT/options.html} for more information.
+#' @inheritParams DT::renderDataTable
+#'
 #' @export
 dtedit <- function(input, output, name, thedata, id,
                    view.cols = names(thedata),
@@ -101,6 +106,7 @@ dtedit <- function(input, output, name, thedata, id,
                    numeric.width = '100px',
                    select.width = '100%',
                    defaultPageLength = 10,
+                   #
                    title.delete = 'Delete',
                    title.edit = 'Edit',
                    title.add = 'New',
@@ -113,17 +119,19 @@ dtedit <- function(input, output, name, thedata, id,
                    show.delete = TRUE,
                    show.update = TRUE,
                    show.insert = TRUE,
-                   show.copy = TRUE,
+                   show.copy   = TRUE,
                    #
                    enable.delete = TRUE,
                    enable.update = TRUE,
                    enable.insert = TRUE,
                    enable.copy   = TRUE,
                    #
-                   callback.delete = function(data, row) { },
-                   callback.update = function(data, olddata, row) { },
-                   callback.insert = function(data, row) { },
-                   callback.replace = function(data) {data},
+                   callback.delete  = function(data, row,          input) { },
+                   callback.update  = function(data, olddata, row, input) { },
+                   callback.insert  = function(data, row,          input) { },
+                   callback.replace = function(data,               input) {data},
+                   #
+                   callback.filter  = function(data, input) {data},
                    click.time.threshold = 2, # in seconds
                    datatable.options = list(pageLength=defaultPageLength),
                    colnames
@@ -152,8 +160,9 @@ dtedit <- function(input, output, name, thedata, id,
   result$view.cols <- view.cols
   result$edit.cols <- edit.cols
   result$dt.name <- DataTableName
+  result$dt.proxy <- dt.proxy <- DT::dataTableProxy(DataTableName)
 
-  dt.proxy <- DT::dataTableProxy(DataTableName)
+  
 
   selectInputMultiple <- function(...) {
     shiny::selectInput(multiple = TRUE, selectize = selectize, ...)
@@ -295,6 +304,7 @@ dtedit <- function(input, output, name, thedata, id,
         data[,i] <- sapply(data[,i], FUN = function(x) { paste0(x, collapse = ', ') })
       }
     }
+    data <- callback.filter(data, input)
     DT::replaceData(proxy, data, ...)
   }
   
@@ -330,7 +340,7 @@ dtedit <- function(input, output, name, thedata, id,
       }
     }
     tryCatch({
-      callback.data <- callback.insert(data = newdata, row = row)
+      callback.data <- callback.insert(data = newdata, row = row, input = input)
       if(!is.null(callback.data) & is.data.frame(callback.data)) {
         result$thedata <- callback.data
       } else {
@@ -379,6 +389,7 @@ dtedit <- function(input, output, name, thedata, id,
     if(!is.null(row) && row > 0) {
       shiny::showModal(editModal(row))
     } else {
+      browser()
       shiny::showNotification('No row selected for update.', type='error')
     }
   })
@@ -408,7 +419,8 @@ dtedit <- function(input, output, name, thedata, id,
       tryCatch({
         callback.data <- callback.update(data = newdata,
                                          olddata = result$thedata,
-                                         row = row)
+                                         row = row,
+                                         input = input)
         if(!is.null(callback.data) & is.data.frame(callback.data)) {
           result$thedata <- callback.data
         } else {
@@ -458,7 +470,7 @@ dtedit <- function(input, output, name, thedata, id,
   observeEvent(input[[paste0(name, '_delete')]], {
     row <- input[[paste0(name, '_dt_rows_selected')]]
     if(!is.null(row) && row > 0) {
-      newdata <- callback.delete(data = result$thedata, row = row)
+      newdata <- callback.delete(data = result$thedata, row = row, input=input)
       if(!is.null(newdata) & is.data.frame(newdata)) {
         result$thedata <- newdata
       } else {
